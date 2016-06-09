98872ade47431dd30649ed88bb3fa59c02ef70d6
torben <t.sickert[~at~]gmail.com>
2016-06-09 16:13:15 +0200

    Automatic update to new version.

--- source/index.js
+++ source/index.js
@@ -38,6 +38,8 @@ const context:Object = (():Object => {
 })()
 if (!('document' in context) && 'context' in $)
     context.document = $.context
+// TODO
+// require('offline-plugin/runtime').install()
 // region plugins/classes
 /**
  * This plugin holds all needed methods to extend a whole documentation site.
