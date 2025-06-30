7ac600404bea0c8d1938668d7302d5650a39e7f5
thaibault <info@torben.website>
2025-06-30 12:44:18 +0200

    Automatic update to new version.

---
+++ source/renderMarkdown.cjs
@@ -0,0 +1,28 @@
+import {Marked} from 'marked'
+import {gfmHeadingId} from 'marked-gfm-heading-id'
+import {markedHighlight} from 'marked-highlight'
+import highlightJSModule from 'highlight.js'
+import {markedXhtml} from 'marked-xhtml'
+
+const {getLanguage, highlight} = highlightJSModule
+
+const marked = new Marked(
+    markedHighlight({
+        // A string to prefix the className in a <code> block. Useful for syntax highlighting.
+        langPrefix: 'language-',
+        highlight: (code, lang, info) =>
+            highlight(
+                code, {language: getLanguage(lang) ? lang : 'plaintext'}
+            ).value
+    })
+)
+
+const {parse, use} = marked
+
+use(self.module.preprocessor.ejs.options.locals.MARKED_OPTIONS)
+// Include an id attribute when emitting headings (h1, h2, h3, etc).
+use(gfmHeadingId({prefix: 'doc-'}))
+// Favors self-closing xhtml tags.
+use(markedXhtml())
+
+return parse
